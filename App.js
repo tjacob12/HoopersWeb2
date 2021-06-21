c1a1447d31faacb1c7a4e6c87e1fcbcfd6596f82
tjacob12 <83917438+tjacob12@users.noreply.github.com>
2021-06-21 09:44:12 -0500

    Add files via upload

--- App.js
+++ App.js
@@ -1,61 +1,61 @@
  import 'bootstrap/dist/css/bootstrap.css';
 import Container from 'react-bootstrap/Container'
-// import Row from 'react-bootstrap/Row';
-// import Col from 'react-bootstrap/Col';
+ import Row from 'react-bootstrap/Row';
+ import Col from 'react-bootstrap/Col';
+ //import Button from 'react-bootstrap/Button';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
         <footer>
-        <Container fluid>
-        <div className = 'hoopersorange'>
-          {/* <Row xs={5}>
-            <Col xs={4}> */}
-              <div className='email-address'>
-             <b>get in touch</b><br></br>
-            info@hoopers.club 
+          <Container fluid className = 'hoopersorange pt-4 fixed-bottom'>
+          <Row xs={4}>
+            <Col xs={4} className='sec1'>
+              get in touch<br></br>
+              <p className='font-weight-light'>info@hoopers.club</p>
+
+              
+                address<br></br>
+                <p className='font-weight-light'>Rua da Prata,80,1,1100-420<br></br>
+                Lisboa,Portugal
+              </p>
+            </Col>
+            <Col xs={1} className = 'sec2'>
+              <p>follow us<br></br>
+              
+                instagram<br></br>
+                youtube<br></br>
+                twitter<br></br>
+                facebook<br></br>
+                spotify
+              </p>
+            </Col>
+            <Col className='sec3'>
+              <p>
+                careers at hoopers<br></br>
+                terms and conditions<br></br>
+                privacy policy<br></br>
+                cookies policy<br></br>
+                about us
+              </p>
+            </Col>
+            <Col className='sec4'>
+              <p>join our mailing list</p>
+              <div className='underline'>
+                 <input className ='subscribe' type='text' placeholder='email'></input>
               </div>
-        
+              <p></p>
+             
+          <button type="button" className="btn btn-warning btn-md">SUBSCRIBE</button>
+            </Col>
+            <Col xs={11} className='text-center'>
+                <b>&copy;2021 Hoopers - All Rights Reserved</b>
+            </Col>
+          </Row>
 
-              <div className = 'location'>
-            <p><b> address</b> <br></br>
-              Rus da Prata,80,1, 1100-420 <br></br>
-              Lisboa,Portugal
-              </p> 
-              </div>
-            {/* </Col>
-          </Row> */}
-         {/* <Row className = 'social media text-center'>
-           <Col xs={8}> */}
-        <div className= 'social-media'>
-           <b>follow us</b><br></br>
-          <nav>
-            <a href='#instagram'>instagram</a><br></br>
-            <a href='#youtube'>youtube</a><br></br>
-            <a href='#twitter'>twitter</a><br></br>
-            <a href='#facebook'>facebook</a><br></br>
-            <a href='#spotify'>spotify</a>
-          </nav>
-         </div>
-
-                {/* <div className ='other'>
-                    <p>
-                      careers at hoopers <br></br>
-                      terms and conditions <br></br>
-                      privacy policy <br></br>
-                      cookies policy <br></br>
-                      about us
-                    </p>
-               </div> */}
-
-              {/* <div className='subscribe'>
-                <p>
-                join our mailing list <br></br></p>
-              </div> */}
-            </Container>
-            </div>
-          </footer>
+          </Container>
+        </footer>
           </div>
 
 
